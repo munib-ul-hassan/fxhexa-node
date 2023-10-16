6fc27bf358bc6d4149132fb1c65f3152cb9176aa
munib-ul-hassan <munibkhatri123@gmail.com>
2023-10-17 00:10:12 +0500

    remove env var ans set key directly

--- src/Controller/ForexController.js
+++ src/Controller/ForexController.js
@@ -4,7 +4,7 @@ import CustomError from "../Utils/ResponseHandler/CustomError.js";
 const gettickers = async (req, res, next) => {
     try {
 
-        const url = `https://api.polygon.io/v2/snapshot/locale/global/markets/forex/tickers?apiKey=${process.env.apiKey}`
+        const url = `https://api.polygon.io/v2/snapshot/locale/global/markets/forex/tickers?apiKey=x5Vm09UZQ8XJpEL0SIgpKJxaROq8jgeQ`
 
         const data = (await axios.get(url)).data
         return res.json({
@@ -20,7 +20,7 @@ const gettickers = async (req, res, next) => {
 const getgraph = async (req, res, next) => {
     try {
         const { series, from,to, ticket,range } = req.query
-        const url = `https://api.polygon.io/v2/aggs/ticker/${ticket}/range/${range}/${series}/${from}/${to}?apiKey=${process.env.apiKey}`
+        const url = `https://api.polygon.io/v2/aggs/ticker/${ticket}/range/${range}/${series}/${from}/${to}?apiKey=x5Vm09UZQ8XJpEL0SIgpKJxaROq8jgeQ`
 
         const data = (await axios.get(url)).data
         return res.json({
--- src/Controller/stocksController.js
+++ src/Controller/stocksController.js
@@ -4,7 +4,7 @@ const getrealTimeData = async (req, res) => {
     try {
         const { series, min, ticket } = req.query
         let url = `https://www.alphavantage.co/query?function=${series}&symbol=${ticket}&interval=${min}min&apikey=${process.env.alphaAPIKEY}`
-        // const url =`https://api.polygon.io/v3/trades/${ticket}?apiKey=${process.env.apiKey}`
+        // const url =`https://api.polygon.io/v3/trades/${ticket}?apiKey=x5Vm09UZQ8XJpEL0SIgpKJxaROq8jgeQ`
         console.log(url)
 
         const data = (await axios.get(url)).data
@@ -27,7 +27,7 @@ const getStocks = async (req, res) => {
         const { keyword } = req.query
         // let url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${process.env.alphaAPIKEY}`
         
-        const url =`https://api.polygon.io/v3/reference/tickers?apiKey=${process.env.apiKey}&search=${keyword}`
+        const url =`https://api.polygon.io/v3/reference/tickers?apiKey=x5Vm09UZQ8XJpEL0SIgpKJxaROq8jgeQ&search=${keyword}`
         console.log(url)
         const data = (await axios.get(url)).data
         return res.json({
@@ -48,7 +48,7 @@ const getStocks = async (req, res) => {
 //     try {
 //         const { asset ,local} = req.query
 
-//         let url = `https://api.polygon.io/v3/reference/exchanges?asset_class=${asset}&locale=${local}&apiKey=${process.env.apiKey}`
+//         let url = `https://api.polygon.io/v3/reference/exchanges?asset_class=${asset}&locale=${local}&apiKey=x5Vm09UZQ8XJpEL0SIgpKJxaROq8jgeQ`
 //         console.log(url)
 //         const data = (await axios.get(url)).data
 //         return res.json({
