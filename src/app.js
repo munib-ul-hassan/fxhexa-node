fa0fbaaad2514a890ae592f2fe3bc410dcba140e
Munib ul hassan <46391010+munib-ul-hassan@users.noreply.github.com>
2024-04-09 04:03:45 +0500

    update server.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 
 import { createServer } from "http"; // for local server
 import { app } from "./appsub.js";
-// import   {Server} from "socket.io";
+import ios from 'socket.io';
+
+const { Server, Socket } = ios;
 // import * as socketIo from 'socket.io' 
 import { socketEventListner } from "./socketEventListener.js";
 // import { socket_connection } from "./server.js";
-import {Server, Socket} from 'socket.io'; 
+// import {Server, Socket} from 'socket.io'; 
 // const io = new Server(server);
 const httpServer = createServer(app); // for local without https
-export const io = new Server(httpServer); // for local
+// export const io = new Server(httpServer); // for local
 // io = socketIo(httpServer)
 
-io.on("connection", (s)=>{
-s.emit("data","============")
-});
+// io.on("connection", (s)=>{
+// s.emit("data","============")
+// });
 // socket_connection()
 
 const port = process.env.PORT || 9000;
